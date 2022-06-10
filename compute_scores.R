02d762d504f241dc271bcc381bce59a749052e9b
dwolffram <25206417+dwolffram@users.noreply.github.com>
2022-06-10 16:57:58 +0200

    script to load submissions and precompute scores

---
+++ compute_scores.R
@@ -0,0 +1,29 @@
+source("load_truth.R")
+
+# simple implementation of quantile score
+qs <- function(q, y, alpha) {
+  (as.numeric(y < q) - alpha) * (q - y)
+}
+
+score <- function(prediction, observation, type, quantile) {
+  if (type == "mean") {
+    return(abs(prediction - observation))
+  } else if (type == "quantile") {
+    return(qs(prediction, observation, quantile))
+  }
+}
+
+END_DATE <- "2022-03-01"
+EVAL_DATE <- "2022-06-01"
+
+df <- read_csv(paste0("data/submissions_", END_DATE, ".csv.gz"))
+df_truth <- load_truth(as_of = EVAL_DATE)
+
+df <- df %>%
+  left_join(df_truth, by = c("location", "age_group", "target_end_date" = "date"))
+
+df <- df %>%
+  rowwise() %>%
+  mutate(score = score(value, truth, type, quantile))
+
+write_csv(df, paste0("data/scores_", END_DATE, "_", EVAL_DATE, ".csv.gz"))
