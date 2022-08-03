413e9ba767bbed9eff3d978a77d4e962b82e8de4
dwolffram <25206417+dwolffram@users.noreply.github.com>
2022-08-03 15:33:25 +0200

    fill in retrospective submissions

--- load_submissions.R
+++ load_submissions.R
@@ -37,7 +37,7 @@ write_csv(df, paste0("data/submissions_", END_DATE, ".csv.gz"))
 PATH_RETRO <- "../hospitalization-nowcast-hub/data-processed_retrospective/"
 files <- list.files(PATH_RETRO, pattern = "*.csv", recursive = TRUE)
 
-df_files <- data.frame(path = files) %>%
+df_files2 <- data.frame(path = files) %>%
   mutate(model = str_split(path, "/", simplify = TRUE)[, 1]) %>%
   mutate(forecast_date = str_extract(path, "\\d{4}-\\d{2}-\\d{2}")) %>%
   filter(
@@ -45,7 +45,7 @@ df_files <- data.frame(path = files) %>%
     forecast_date <= END_DATE
   )
 
-df_files <- df_files %>% 
+df_files2 <- df_files2 %>% 
   filter(str_detect(path, "Check", negate = TRUE))
 
 # load all submissions into one dataframe in long format
@@ -63,3 +63,45 @@ for (i in 1:nrow(df_files)) {
 }
 
 write_csv(df, paste0("data/submissions_", END_DATE, "_retrospective.csv.gz"))
+
+
+### Fill in retrospective submissions
+
+df_files <- df_files %>% 
+  mutate(retrospective = FALSE)
+
+df_files2 <- df_files2 %>% 
+  mutate(retrospective = TRUE)
+
+d <- bind_rows(df_files, df_files2)
+
+# d %>% 
+#   count(model, forecast_date) %>% 
+#   filter(n > 1)
+
+d <- d %>% 
+  group_by(model, forecast_date) %>% 
+  mutate(duplicated = n() > 1)
+
+d <- d %>% 
+  filter(!(!retrospective & duplicated))
+
+d <- d %>% 
+  mutate(folder = if(retrospective) PATH_RETRO else HUB_PATH)
+
+# load all submissions into one dataframe in long format
+df <- data.frame()
+pb <- txtProgressBar(min = 0, max = nrow(d), style = 3)
+for (i in 1:nrow(d)) {
+  row <- d[i, ]
+  df_temp <- read_csv(paste0(row$folder, row$path),
+                      show_col_types = FALSE, progress = FALSE
+  )
+  df_temp$model <- row$model
+  df_temp$retrospective = row$retrospective
+  df <- bind_rows(df, df_temp)
+  
+  setTxtProgressBar(pb, i)
+}
+
+write_csv(df, paste0("data/submissions_", END_DATE, "_filled.csv.gz"))
