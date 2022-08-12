d88487b58f449b64980b7655a143c7b555ed4f50
dwolffram <25206417+dwolffram@users.noreply.github.com>
2022-08-12 17:07:49 +0200

    replaced erroneus nowcasts and recomputed scores

--- check_submissions.R
+++ check_submissions.R
@@ -1,3 +1,120 @@
+library(tidyverse)
+source("utils.R")
+df <- read_csv("data/submissions_2021-11-22_2022-04-29.csv.gz")
+
+# Add baseline
+df_baseline <- read_csv(paste0("data/submissions_KIT-frozen_baseline.csv.gz")) %>% 
+  mutate(retrospective = FALSE)
+df <- bind_rows(df, df_baseline)
+
+df$model <- factor(df$model,
+                   levels = sort(unique(df$model)),
+                   labels = SHORT_NAMES)
+
+
+# View(subset(df, type == "mean" &
+#             forecast_date == "2022-04-05" &
+#             location == "DE-HH"))
+# 
+# View(subset(df, 
+#               forecast_date == "2022-04-05" &
+#               location == "DE-HH"))
+# 
+# View(df %>% 
+#        filter(model == "RKI" &
+#               value > 1000000))
+
+
+# problematic rows
+df_error <- df %>% 
+  filter(model == "RKI",
+         value > 1000000 | is.na(value)) %>% 
+  select(c(location, age_group, forecast_date, type, quantile))
+
+df_replacement <- df %>% 
+  filter(model == "RKI",
+         target %in% c("0 day ahead inc hosp", "-1 day ahead inc hosp"))
+
+df_fixed <- df_error %>% 
+  left_join(df_replacement) %>% 
+  group_by(location, age_group, forecast_date, type, quantile, pathogen, model, retrospective) %>% 
+  summarize(target_end_date = max(target_end_date),
+            target = max(target),
+            value = min(value, na.rm = TRUE))
+
+nrow(df)
+
+df <- df %>% 
+  filter(!(model == "RKI" & (value > 1000000 | is.na(value))))
+df <- bind_rows(df, df_fixed)
+
+nrow(df)
+
+
+# some 0 day ahead nowcasts are unrealisticly large (>1 bio)
+# we replace them with the respective -1 day ahead nowcasts
+fix_rki_errors <- function(df) {
+  df_error <- df %>% 
+    filter(model == "RKI",
+           value > 1000000 | is.na(value)) %>% 
+    select(c(location, age_group, forecast_date, type, quantile))
+  
+  df_replacement <- df %>% 
+    filter(model == "RKI",
+           target %in% c("0 day ahead inc hosp", "-1 day ahead inc hosp"))
+  
+  df_fixed <- df_error %>% 
+    left_join(df_replacement) %>% 
+    group_by(location, age_group, forecast_date, type, quantile, pathogen, model, retrospective) %>% 
+    summarize(target_end_date = max(target_end_date),
+              target = max(target),
+              value = min(value, na.rm = TRUE))
+  
+  df <- df %>% 
+    filter(!(model == "RKI" & (value > 1000000 | is.na(value))))
+  df <- bind_rows(df, df_fixed)
+  
+  return(df)
+}
+
+
+View(df %>%
+       filter(model == "RKI" &
+              value > 1000000))
+
+df <- fix_rki_errors(df)
+
+View(df %>%
+       filter(model == "RKI" &
+                value > 1000000))
+
+a <- df %>% 
+  filter(value <= 1000000)
+
+nrow(df)
+
+b <- subset(df, value >  1000000)
+c <- subset(df, value < 1000000)
+
+nrow(b) + nrow(c)
+
+nrow(df)
+
+
+
+
+View(subset(df, model == "RKI-weekly_report" & forecast_date == "2021-11-24"))
+
+d <- df %>% 
+  group_by(model, forecast_date) %>% 
+  mutate(duplicated = n() > 1)
+
+# if duplicated (prospective + retrospective) remove prospective submission
+d <- d %>% 
+  filter(!(!retrospective & duplicated))
+
+
+
 df %>% 
   count(model)
 
--- compute_scores.R
+++ compute_scores.R
@@ -1,4 +1,5 @@
 source("load_truth.R")
+source("utils.R")
 
 # Quantile score
 qs <- function(q, y, alpha) {
@@ -22,13 +23,21 @@ EVAL_DATE <- "2022-08-08" # date of truth data
 df <- read_csv(paste0("data/submissions_", START_DATE, "_", END_DATE, ".csv.gz"))
 
 # Add baseline
-df_baseline <- read_csv(paste0("data/submissions_KIT-frozen_baseline.csv.gz")) %>% 
+df_baseline <- read_csv(paste0("data/submissions_KIT-frozen_baseline.csv.gz")) %>%
   mutate(retrospective = FALSE)
 df <- bind_rows(df, df_baseline)
 
+df$model <- factor(df$model,
+  levels = sort(unique(df$model)),
+  labels = SHORT_NAMES
+)
+
+# replace erroneus nowcasts
+df <- fix_rki_errors(df)
+
 # Add median separately
-df_median <- df %>% 
-  filter(quantile == 0.5) %>% 
+df_median <- df %>%
+  filter(quantile == 0.5) %>%
   mutate(type = "median")
 df <- bind_rows(df, df_median)
 
@@ -41,21 +50,21 @@ df <- df %>%
   rowwise() %>%
   mutate(score = score(value, truth, type, quantile))
 
-df <- df %>% 
+df <- df %>%
   select(-c(pathogen, retrospective))
 
 
-scores_baseline <- df %>% 
+scores_baseline <- df %>%
   filter(model == "KIT-frozen_baseline")
 
 write_csv(scores_baseline, paste0("data/scores_", START_DATE, "_", END_DATE, "_baseline.csv.gz"))
 
-df <- df %>% 
+
+
+df <- df %>%
   filter(model != "KIT-frozen_baseline")
 
 write_csv(df, paste0("data/scores_", START_DATE, "_", END_DATE, ".csv.gz"))
-
-
 
 # Aggregate scores
 df <- bind_rows(df, scores_baseline)
@@ -65,25 +74,31 @@ df <- df %>%
 
 write_csv(df, paste0("data/scores_", START_DATE, "_", END_DATE, "_aggregated.csv.gz"))
 
-
+s <- df %>%
+  filter(
+    location == "DE",
+    age_group == "00+"
+  ) %>%
+  group_by(model, type) %>%
+  summarize(score = mean(score))
 
 ### FROZEN BASELINE
 
 # df <- read_csv(paste0("data/submissions_KIT-frozen_baseline.csv.gz"))
 # df_truth <- load_truth(as_of = EVAL_DATE)
-# 
+#
 # df <- df %>%
 #   left_join(df_truth, by = c("location", "age_group", "target_end_date" = "date"))
-# 
+#
 # df <- df %>%
 #   rowwise() %>%
 #   mutate(score = score(value, truth, type, quantile))
-# 
+#
 # write_csv(df, paste0("data/scores_KIT-frozen_baseline.csv.gz"))
-# 
+#
 # # Aggregate scores
 # df <- df %>%
 #   group_by(model, location, age_group, target, type) %>%
 #   summarize(score = mean(score))
-# 
+#
 # write_csv(df, paste0("data/scores_KIT-frozen_baseline_aggregated.csv.gz"))
