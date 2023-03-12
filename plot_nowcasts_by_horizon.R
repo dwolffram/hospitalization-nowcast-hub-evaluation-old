bb7a33ef0a05a5f9b134a696afeac709f44aaa62
dwolffram <25206417+dwolffram@users.noreply.github.com>
2023-03-12 10:03:40 +0100

    updated plot of nowcasts

--- plot_nowcasts_by_horizon.R
+++ plot_nowcasts_by_horizon.R
@@ -34,7 +34,7 @@ df1$age_group <- factor(df1$age_group, levels = c("00+", "00-04", "05-14", "15-3
 
 
 alphas <- setNames(c(0.7, 0.4), c("50%", "95%"))
-line_colors <- setNames(c("firebrick3", "gray"), c("Latest", "Frozen"))
+line_colors <- setNames(c("firebrick3", "gray"), c("Final", "At time of nowcast"))
 
 df1 <- df1 %>% 
   mutate(model = fct_relevel(model, c(
@@ -45,7 +45,7 @@ df1 <- df1 %>%
 
 ggplot(df1) +
   facet_wrap("model", scales = "fixed", ncol = 3) +
-  geom_line(aes(x = target_end_date, y = frozen_value, color = "Frozen"), linetype = "solid") +
+  geom_line(aes(x = target_end_date, y = frozen_value, color = "At time of nowcast"), linetype = "solid") +
   geom_ribbon(aes(x = target_end_date, ymin = quantile_0.025, ymax = quantile_0.975, alpha = "95%"),
               fill = "skyblue3"
   ) +
@@ -55,14 +55,15 @@ ggplot(df1) +
   geom_line(aes(x = target_end_date, y = quantile_0.5),
             color = "royalblue4", size = 0.25, linetype = "solid"
   ) +
-  geom_line(aes(x = target_end_date, y = truth, color = "Latest")) +
+  geom_line(aes(x = target_end_date, y = truth, color = "Final")) +
   
   labs(x = NULL, y = "7-day hospitalization incidence", title = paste("Horizon:", LEAD_TIME, "days (national level, all age groups)"))+
   scale_alpha_manual(name = "Nowcasts with \nprediction intervals:", values = alphas,
                      guide = guide_legend(order = 2, title.position = "top", title.hjust = 0)) +
   scale_color_manual(name = "Truth", values = line_colors,
                      guide = guide_legend(order = 1, title.position = "top", title.hjust = 0)) +
-  expand_limits(y = 0) +
-  theme_bw()
+  # expand_limits(y = 0) +
+  theme_bw() +
+  coord_cartesian(ylim = c(NA, 17500))
 
 ggsave(paste0("figures/nowcasts_", LEAD_TIME, "d.pdf"), width = 300, height = 200, unit = "mm", device = "pdf")
