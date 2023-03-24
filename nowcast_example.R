7857f8b61b3e3dbf611ce2617f61e2de35b925e8
dwolffram <25206417+dwolffram@users.noreply.github.com>
2023-03-24 11:37:30 +0100

    updated nowcast example plot

--- nowcast_example.R
+++ nowcast_example.R
@@ -198,3 +198,47 @@ dfs %>%
   expand_limits(x = as.Date("2022-06-10"), y = 12750)
 
 ggsave("figures/nowcast_example_frozen.pdf", width = 300, height = 200, unit = "mm", device = "pdf")
+
+
+######
+
+dfs %>%
+  filter(date >= "2021-07-01", date <= "2022-06-01", as_of %in% c(dates, "2022-05-01", "unrevised, \ninitial reports")) %>%
+  ggplot() +
+  geom_vline(data = data.frame(date = as.Date(dates[-4])), 
+             aes(xintercept = date, linetype = "Date of nowcast"), color = "black") +
+  geom_line(aes(x = date, y = value_7d, color = as_of)) +
+  geom_ribbon(data = dfs1, 
+              aes(x = target_end_date, ymin = quantile_0.025, ymax = quantile_0.975, 
+                  group = as_of, alpha = "95%"), 
+              fill = "skyblue3") +
+  geom_ribbon(data = dfs1, 
+              aes(x = target_end_date, ymin = quantile_0.25, ymax = quantile_0.75, 
+                  group = as_of, alpha = "50%"), 
+              fill = "skyblue3") +
+  geom_line(data = dfs1, aes(x = target_end_date, y = quantile_0.5, group = as_of), 
+            color = "skyblue3", linetype = "solid") +
+  scale_x_date(date_breaks = "2 months", date_labels = "%b %Y", expand = c(0.02, 0)) +
+  scale_color_manual(values = c("#009E73", "#E69F00", "#D55E00","#000000", "gray"), guide = guide_legend(order = 1)) +
+  scale_linetype_manual(name = NULL, values = c("Date of nowcast" = "dotted")) +
+  theme_bw()  +   
+  labs(x = NULL, 
+       y = "7-day hospitalization incidence",
+       color = "Data version") +
+  scale_alpha_manual(name = "Nowcasts with \nprediction intervals:", values = alphas) +
+  theme(legend.position = "right",
+        legend.title = element_text(size = 8),
+        legend.text = element_text(size = 8),
+        legend.key.size = unit(0.65, "lines"),
+        axis.title.y = element_text(size = 8),
+        axis.text = element_text(size = 7),
+        axis.ticks = element_line(colour = "black", size = 0.25),
+        panel.grid.major = element_line(size = 0.15),
+        panel.grid.minor = element_line(size = 0.1),
+        plot.margin = unit(c(1, 1.5, 0, 1.5), "pt"), 
+        legend.margin = margin(0, 0, 0, 5),
+        legend.box.spacing = unit(0, "pt"),
+        legend.background = element_rect(fill='transparent')) + 
+  expand_limits(x = as.Date("2022-06-10"), y = 12750)
+
+ggsave("figures/nowcast_example_frozen.pdf", width = 164, height = 55, unit = "mm", device = "pdf")
