49301804bfe038023738c43f03195749080aa2fa
dwolffram <25206417+dwolffram@users.noreply.github.com>
2023-03-24 10:35:16 +0100

    updated fraction plots

--- plot_fraction.R
+++ plot_fraction.R
@@ -4,37 +4,6 @@ source("load_truth.R")
 
 Sys.setlocale("LC_ALL", "C")
 
-frozen_sum <- function(df, indices_frozen) {
-  if (nrow(df) != 7) {
-    return(NA)
-  } else {
-    values <- df %>%
-      ungroup() %>%
-      select(any_of(paste0("value_", 0:(ncol(indices_frozen) - 1), "d")))
-    
-    sum(values[indices_frozen[, 1:ncol(values)]])
-  }
-}
-
-load_frozen_truth <- function(lead_time = 0, start_date = "2021-11-01") {
-  indices_frozen <- make_frozen_indices(lead_time)
-  
-  df <- read_csv("../hospitalization-nowcast-hub/data-truth/COVID-19/COVID-19_hospitalizations.csv",
-                 show_col_types = FALSE
-  ) %>%
-    filter(date >= start_date) %>% 
-    rename(value_81d = `value_>80d`)
-  
-  df <- df %>%
-    group_by(location, age_group) %>%
-    run_by(idx = "date", k = "7 days") %>%
-    mutate(frozen_value = runner(x = ., f = function(x) {
-      frozen_sum(x, indices_frozen)
-    })) %>%
-    select(c(date, location, age_group, frozen_value)) %>%
-    drop_na()
-}
-
 # t <- load_frozen_truth(81, "2021-11-01")
 # write_csv(t, paste0("data/truth_frozen/truth_frozen_", 81, "d.csv"))
 # 
@@ -88,27 +57,7 @@ df_age <- df %>%
 
 
 ### PERCENTAGE (OVER TIME)
-
 # National
-df_fraction_national <- df_national %>%
-  subset(d %in% c(0, 2, 7, 14, 28, 49, 70, "70+")) %>%
-  group_by(location, date) %>%
-  mutate(value = value / max(value))
-
-ggplot(df_fraction_national, aes(x = date, y = value, fill = fct_reorder(d, value, .desc = TRUE)), group = d) +
-  facet_wrap("location", scales = "fixed") +
-  geom_area(position = "identity", alpha = 0.5) +
-  scale_fill_viridis_d(direction = 1) +
-  theme_bw() +
-  labs(
-    x = NULL,
-    y = "Fraction of final report",
-    fill = "Days after\ninitial report"
-  ) +
-  scale_x_date(date_breaks = "2 months", minor_breaks = "1 month", date_labels = "%b %Y")
-
-ggsave("figures/fraction_time_national.pdf", width = 150, height = 120, unit = "mm", device = "pdf")
-
 
 df_fraction_national <- df_national %>%
   subset(d %in% c(0, 2, 7, 14, 28, 49, 70, "70+")) %>% 
@@ -138,18 +87,15 @@ p1 <- ggplot(df_fraction_national, aes(x = date, y = value, fill = d, color = d)
     y = "Fraction of final report",
     fill = "Days after\ninitial report"
   ) +
-  scale_x_date(date_breaks = "1 months", minor_breaks = "1 month", date_labels = "%b %Y", expand = c(0.1, 0))
+  scale_x_date(date_breaks = "1 months", minor_breaks = "1 month", date_labels = "%b", 
+               expand = c(0.05, 0))
 
 # ggsave("figures/fraction_time_national_new.pdf", width = 150, height = 120, unit = "mm", device = "pdf")
 
 
-
-
-
-
-
 ### PERCENTAGE (BY STRATUM)
 # States
+
 df_fraction <- df_states %>%
   subset(d %in% c(0, 2, 7, 14, 28, 49, 70, "70+")) %>% 
   group_by(location, d) %>%
@@ -181,34 +127,33 @@ p2 <- ggplot(df_fraction, aes(x = fct_reorder(location, value, .fun = min, .desc
 
 # Age
 
-# df_fraction <- df_age %>%
-#   subset(d %in% c(0, 2, 7, 14, 28, 49, 70, "70+")) %>% 
-#   group_by(age_group, d) %>%
-#   summarize(value = sum(value)) %>%
-#   group_by(age_group) %>%
-#   mutate(value = value / max(value))
-# 
-# df_fraction <- df_fraction %>%
-#   mutate(d = fct_reorder(d, value, .desc = TRUE)) %>%
-#   arrange(d)
-# 
-# ggplot(df_fraction, aes(x = age_group, y = value, fill = d)) +
-#   geom_bar(stat = "identity", position = "identity", alpha = 1) +
-#   # scale_fill_viridis_d(direction = 1) +
-#   scale_fill_brewer(palette = "RdYlGn") +
-#   theme_bw(base_size = 11) +
-#   theme(
-#     panel.grid.major = element_line(size = 0.05),
-#     panel.grid.minor = element_line(size = 0.05)
-#   ) +
-#   labs(
-#     x = NULL,
-#     y = "Fraction of final report",
-#     fill = "Days after\ninitial report"
-#   )
-# 
-# ggsave("figures/fraction_time_age.pdf", width = 300, height = 200, unit = "mm", device = "pdf")
+df_fraction <- df_age %>%
+  subset(d %in% c(0, 2, 7, 14, 28, 49, 70, "70+")) %>%
+  group_by(age_group, d) %>%
+  summarize(value = sum(value)) %>%
+  group_by(age_group) %>%
+  mutate(value = value / max(value))
 
+df_fraction <- df_fraction %>%
+  mutate(d = fct_reorder(d, value, .desc = TRUE)) %>%
+  arrange(d)
+
+p4 <- ggplot(df_fraction, aes(x = age_group, y = value, fill = d)) +
+  geom_bar(stat = "identity", position = "identity", alpha = 1) +
+  # scale_fill_viridis_d(direction = 1) +
+  scale_fill_brewer(palette = "RdYlGn") +
+  theme_bw(base_size = 11) +
+  theme(
+    panel.grid.major = element_line(size = 0.05),
+    panel.grid.minor = element_line(size = 0.05)
+  ) +
+  labs(
+    x = NULL,
+    y = "Fraction of final report",
+    fill = "Days after\ninitial report"
+  )
+
+# ggsave("figures/fraction_age.pdf", width = 300, height = 200, unit = "mm", device = "pdf")
 
 
 ### Weekday
@@ -243,16 +188,32 @@ p3 <- ggplot(df_fraction, aes(x = weekday, y = value, fill = d)) +
   )
 
 
-p1 + theme(legend.position = "none", aspect.ratio = 1) + 
-  p2 + facet_grid("" ~ "States") + theme(legend.position = "none", 
-                                         aspect.ratio = 1,
-                                         strip.background.y = element_blank(),
-                                         strip.text.y = element_blank()) +
-  p3 + facet_grid("" ~ "Weekdays") + theme(aspect.ratio = 1,
-                                         strip.background.y = element_blank(),
-                                         strip.text.y = element_blank())
+p1 + facet_grid( ~ "National level") + theme(legend.position = "none", 
+                                             axis.text.y = element_text(size = 6)) +
+  p2 + facet_grid( ~ "States") + ylab(NULL) + theme(legend.position = "none", 
+                                                    axis.text.y = element_blank()) +
+  p4 + facet_grid( ~ "Age groups") + ylab(NULL) + theme(legend.position = "none", 
+                                                        axis.text.y = element_blank()) +
+  p3 + facet_grid( ~ "Weekdays") + ylab(NULL) + theme(legend.position = "right", 
+                                                      axis.text.y = element_blank()) +
+  plot_layout(widths = c(1.8, 1.8, 1.25, 1)) &
+  theme(legend.title = element_text(size = 6), 
+        legend.text  = element_text(size = 6),
+        legend.key.size = unit(0.4, "lines"),
+        axis.title.y = element_text(size = 7),
+        axis.text.x = element_text(size = 4),
+        strip.text = element_text(size = 8),
+        axis.ticks = element_line(colour = "black", size = 0.25),
+        panel.grid.major = element_line(size = 0.15),
+        panel.grid.minor = element_line(size = 0.1),
+        plot.margin = unit(c(1, 1.5, 0, 1.5), "pt"), 
+        legend.margin = margin(0, 0, 0, 5),
+        legend.box.spacing = unit(0, "pt"),
+        legend.background = element_rect(fill='transparent'))
 
-ggsave("figures/fractions.pdf", width = 400, height = 120, unit = "mm", device = "pdf")
+# ggsave("figures/fractions4.pdf", width = 400, height = 120, unit = "mm", device = "pdf")
+
+ggsave("figures/fractions.pdf", width = 164, height = 45, unit = "mm", device = "pdf")
 
 
 ### Over time by state
@@ -273,10 +234,58 @@ ggplot(df_fraction_states, aes(x = date, y = value, fill = fct_reorder(d, value,
     y = "Fraction of final report",
     fill = "Days after\ninitial report"
   ) +
-  scale_x_date(date_breaks = "3 months", minor_breaks = "1 month", date_labels = "%b %Y")
+  scale_x_date(date_breaks = "2 months", minor_breaks = "1 month", date_labels = "%b") +
+  theme(legend.title = element_text(size = 7), 
+        legend.text  = element_text(size = 7),
+        legend.key.size = unit(0.4, "lines"),
+        axis.title.y = element_text(size = 8),
+        axis.text = element_text(size = 7),
+        strip.text = element_text(size = 8),
+        axis.ticks = element_line(colour = "black", size = 0.25),
+        panel.grid.major = element_line(size = 0.15),
+        panel.grid.minor = element_line(size = 0.1),
+        plot.margin = unit(c(1, 1.5, 0, 1.5), "pt"), 
+        legend.margin = margin(0, 0, 0, 5),
+        legend.box.spacing = unit(0, "pt"),
+        legend.background = element_rect(fill='transparent'))
 
-ggsave("figures/fraction_time_states.pdf", width = 300, height = 250, unit = "mm", device = "pdf")
+
+# ggsave("figures/fraction_time_states.pdf", width = 164, height = 150, unit = "mm", device = "pdf")
 
 
 
+### Over time by age group
+
+df_fraction_age <- df_age %>%
+  subset(d %in% c(0, 2, 7, 14, 28, 49, 70, "70+")) %>% 
+  group_by(age_group, date) %>%
+  mutate(value = value / max(value))
+
+ggplot(df_fraction_age, aes(x = date, y = value, fill = fct_reorder(d, value, .desc = TRUE)), group = d) +
+  facet_wrap("age_group", scales = "fixed") +
+  geom_area(position = "identity") +
+  # scale_fill_viridis_d(direction = 1) +
+  scale_fill_brewer(palette = "RdYlGn") +
+  theme_bw() +
+  labs(
+    x = NULL,
+    y = "Fraction of final report",
+    fill = "Days after\ninitial report"
+  ) +
+  scale_x_date(date_breaks = "2 months", minor_breaks = "1 month", date_labels = "%b") +
+  theme(legend.title = element_text(size = 7), 
+        legend.text  = element_text(size = 7),
+        legend.key.size = unit(0.4, "lines"),
+        axis.title.y = element_text(size = 8),
+        axis.text = element_text(size = 7),
+        strip.text = element_text(size = 8),
+        axis.ticks = element_line(colour = "black", size = 0.25),
+        panel.grid.major = element_line(size = 0.15),
+        panel.grid.minor = element_line(size = 0.1),
+        plot.margin = unit(c(1, 1.5, 0, 1.5), "pt"), 
+        legend.margin = margin(0, 0, 0, 5),
+        legend.box.spacing = unit(0, "pt"),
+        legend.background = element_rect(fill='transparent'))
+
+# ggsave("figures/fraction_time_age.pdf", width = 164, height = 75, unit = "mm", device = "pdf")
 
