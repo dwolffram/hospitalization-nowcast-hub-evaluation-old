88ade34bc52be6f4a56e74e6275a8d87e4b7cc5b
dwolffram <25206417+dwolffram@users.noreply.github.com>
2023-03-27 11:23:19 +0200

    updated score plots

--- plot_scores.R
+++ plot_scores.R
@@ -28,10 +28,10 @@ plot_scores <- function(type = "quantile", level = "national",
     }
     
     scores <- scores %>%
-      filter(model != "KIT-frozen_baseline")
+      filter(model != "KIT-frozen_baseline") 
     
     ggplot(scores, aes(x = horizon, y = score, color = model)) +
-      geom_line(size = 1) +
+      geom_line() + # size = 1
       scale_color_manual(values = MODEL_COLORS) +
       labs(
         x = "Horizon (days)",
@@ -51,22 +51,33 @@ plot_scores <- function(type = "quantile", level = "national",
     }
     
     scores <- scores %>%
-      filter(model != "KIT-frozen_baseline") 
+      mutate(model = fct_relevel(model, c(
+        "Epiforecasts", "ILM", "KIT-frozen_baseline", "KIT",
+        "LMU", "RIVM", "RKI", "SU", "SZ", "MeanEnsemble", "MedianEnsemble"
+      )))
+    
+    scores <- scores %>%
+      filter(model != "KIT-frozen_baseline") %>% 
+      mutate(model = fct_drop(model, only = "KIT-frozen_baseline"))
     
     if (add_ae) {
       scores_ae <- filter_scores(df, "median", level, by_horizon) %>%
-        filter(model != "KIT-frozen_baseline")
+        filter(model != "KIT-frozen_baseline") %>% 
+        mutate(model = fct_drop(model, only = "KIT-frozen_baseline"))
     }
     
     ggplot() +
       {if (add_ae) geom_point(data = scores_ae, aes(x = model, y = score, fill = model), shape = 23)} +
       geom_bar(data = scores, aes(x = model, y = score, fill = model), stat = "identity") +
-      geom_label(data = scores, aes(x = model, y = 0.5*score, label = sprintf("%0.1f", round(score, digits = 1))),
-                 fill = "white", alpha = 1, hjust = 0.5,
-                 label.r = unit(0.25, "lines"), size = 9 * 5 / 14,
-                 label.padding = unit(0.15, "lines")) +
+      geom_label(
+        data = scores, aes(x = model, y = 0.5 * score, label = sprintf("%0.1f", round(score, digits = 1))),
+        fill = "white", alpha = 1, hjust = 0.5,
+        label.r = unit(0.25, "lines"), size = 5/.pt, # 9 * 5 / 14
+        label.padding = unit(0.15, "lines")
+      ) +
       scale_fill_manual(values = MODEL_COLORS) +
       scale_color_manual(values = MODEL_COLORS) +
+      scale_x_discrete(limits = rev, drop = FALSE) +
       labs(
         y = ylabel,
         x = NULL,
@@ -123,11 +134,34 @@ p5 <- plot_scores("mean", "age", by_horizon = FALSE, relative = TRUE)
 p6 <- plot_scores("mean", "age", by_horizon = TRUE)
 p6b <- plot_scores("mean", "age", by_horizon = TRUE, relative = TRUE)
 
-wrap_elements(p1 + p2 + p2b + plot_annotation(title = "National level") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1)) /
-  wrap_elements(p3 + p4 + p4b + plot_annotation(title = "Average across states") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1)) /
-  wrap_elements(p5 + p6 + p6b + plot_annotation(title = "Average across age groups") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1))
+# wrap_elements(p1 + p2 + p2b + plot_annotation(title = "National level") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1)) /
+#   wrap_elements(p3 + p4 + p4b + plot_annotation(title = "Average across states") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1)) /
+#   wrap_elements(p5 + p6 + p6b + plot_annotation(title = "Average across age groups") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1))
+# 
+# ggsave("figures/scores_mse.pdf", width = 300, height = 350, unit = "mm", device = "pdf")
 
-ggsave("figures/scores_mse.pdf", width = 300, height = 350, unit = "mm", device = "pdf")
+t <- list(theme(
+  plot.title = element_text(size = 8, hjust = 0.5, margin = margin(10, 0, -3, 0), face = "bold"),
+  legend.title = element_text(size = 6), 
+  legend.text  = element_text(size = 5),
+  legend.key.size = unit(0.4, "lines"),
+  axis.title = element_text(size = 7),
+  axis.text = element_text(size = 6),
+  axis.ticks = element_line(colour = "black", size = 0.25),
+  panel.grid.major = element_line(size = 0.15),
+  panel.grid.minor = element_line(size = 0.1),
+  plot.margin = unit(c(2, 3, 2, 2), "pt"), 
+  legend.margin = margin(4, 0, 0, 0),
+  legend.box.spacing = unit(0, "pt"),
+  legend.background = element_rect(fill='transparent')))
+
+
+(p1 + p2 + labs(title = "National level") + p2b) /
+  (p3 + p4 + labs(title = "Average across states") + p4b) /
+  (p5 + p6 + labs(title = "Average across age groups") + p6b) + plot_annotation(theme = theme(plot.margin = margin())) & t 
+
+ggsave("figures/scores_mse.pdf", width = 164, height = 185, unit = "mm", device = "pdf")
+
 
 
 # Absolute error
@@ -142,11 +176,17 @@ p5 <- plot_scores("median", "age", by_horizon = FALSE, relative = TRUE)
 p6 <- plot_scores("median", "age", by_horizon = TRUE)
 p6b <- plot_scores("median", "age", by_horizon = TRUE, relative = TRUE)
 
-wrap_elements(p1 + p2 + p2b + plot_annotation(title = "National level") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1)) /
-  wrap_elements(p3 + p4 + p4b + plot_annotation(title = "Average across states") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1)) /
-  wrap_elements(p5 + p6 + p6b + plot_annotation(title = "Average across age groups") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1))
+# wrap_elements(p1 + p2 + p2b + plot_annotation(title = "National level") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1)) /
+#   wrap_elements(p3 + p4 + p4b + plot_annotation(title = "Average across states") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1)) /
+#   wrap_elements(p5 + p6 + p6b + plot_annotation(title = "Average across age groups") & theme(plot.title = element_text(hjust = 0.5), aspect.ratio = 1))
+# 
+# ggsave("figures/scores_ae.pdf", width = 300, height = 350, unit = "mm", device = "pdf")
 
-ggsave("figures/scores_ae.pdf", width = 300, height = 350, unit = "mm", device = "pdf")
+(p1 + p2 + labs(title = "National level") + p2b) /
+  (p3 + p4 + labs(title = "Average across states") + p4b) /
+  (p5 + p6 + labs(title = "Average across age groups") + p6b) + plot_annotation(theme = theme(plot.margin = margin())) & t 
+
+ggsave("figures/scores_ae.pdf", width = 164, height = 185, unit = "mm", device = "pdf")
 
 
 
