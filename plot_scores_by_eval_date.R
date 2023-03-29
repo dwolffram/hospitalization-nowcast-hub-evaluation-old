447f69154aecafc2797574e751e729f6d03b4bb5
dwolffram <25206417+dwolffram@users.noreply.github.com>
2023-03-29 10:11:31 +0200

    updated scores by eval date

--- plot_scores_by_eval_date.R
+++ plot_scores_by_eval_date.R
@@ -1,7 +1,7 @@
 library(patchwork)
 source("utils.R")
 
-short_horizons = TRUE
+short_horizons = FALSE
 
 df <- data.frame()
 
@@ -30,9 +30,11 @@ plot_scores_by_eval_date <- function(df, level = "national") {
     filter(level == !!level)
   
   ggplot(df_temp, aes(x = eval_date, y = score, color = model)) +
-    geom_vline(xintercept = as.Date("2022-08-08"), color = "darkgray", linetype = "solid", size = 0.75) +
-    annotate(geom = "label", x = as.Date("2022-08-08"), y = 0.05*max(df_temp$score), label = "8 August 2022", size = 3, color = "darkgray", ) +
-    geom_line(size = 1) +
+    geom_vline(xintercept = as.Date("2022-08-08"), color = "darkgray", linetype = "solid") +
+    annotate(geom = "label", x = as.Date("2022-08-08"), y = 0.05*max(df_temp$score), 
+             label = "8 August 2022", color = "darkgray", size = 6/.pt,
+             label.padding = unit(0.1, "lines")) +
+    geom_line() +
     scale_x_date(date_breaks = "2 months", minor_breaks = "1 month", date_labels = "%b %Y") +
     expand_limits(y = 0) +
     theme_bw() +
@@ -70,7 +72,7 @@ df3 <- df3 %>%
          date >= "2021-11-22")
 
 p4 <- ggplot(df3, aes(x = date, y = truth, color = as_of)) +
-  geom_line(size = 1) +
+  geom_line() +
   scale_x_date(date_breaks = "2 months", date_labels = "%b %Y") +
   scale_color_manual(values = c("black", "darkorange2")) +
   theme_bw() +  
@@ -79,10 +81,29 @@ p4 <- ggplot(df3, aes(x = date, y = truth, color = as_of)) +
        color = "Data version")
 
 
-(p1 + theme(legend.position = "none") + p2 + p3 + theme(legend.position = "none")  + p4)  + plot_layout(ncol = 2)
+t <- list(theme(
+  plot.title = element_text(size = 8, hjust = 0.5, face = "bold"),
+  legend.title = element_text(size = 6), 
+  legend.text  = element_text(size = 5),
+  legend.key.size = unit(0.4, "lines"),
+  axis.title = element_text(size = 6),
+  axis.text = element_text(size = 5),
+  axis.ticks = element_line(colour = "black", size = 0.25),
+  panel.grid.major = element_line(size = 0.15),
+  panel.grid.minor = element_line(size = 0.1),
+  #plot.margin = unit(c(2, 2, 2, 2), "pt"), 
+  #legend.margin = margin(4, 0, 0, 0),
+  #legend.box.spacing = unit(0, "pt"),
+  #legend.background = element_rect(fill='transparent')
+  ))
 
-# ggsave("figures/scores_by_eval_date.pdf", width = 300, height = 200, unit = "mm", device = "pdf")
+
+((p1 + theme(legend.position = "none") + p2 + p3 + theme(legend.position = "none")  + p4)  + plot_layout(ncol = 2) & t) + plot_annotation(theme = theme(plot.margin = margin()))
+
+ggsave("figures/scores_by_eval_date.pdf", width = 164, height = 100, unit = "mm", device = "pdf")
 
 
-p1 + theme(legend.position = "none") + p2 + theme(legend.position = "none")  + p3 & theme(aspect.ratio = 1)
-# ggsave("figures/scores_by_eval_date_7d.pdf", width = 300, height = 90, unit = "mm", device = "pdf")
+(p1 + theme(legend.position = "none") + p2 + theme(legend.position = "none")  + p3 & theme(aspect.ratio = 1) & t &
+  scale_x_date(date_breaks = "2 months", minor_breaks = "1 month", date_labels = "%b")) + plot_annotation(theme = theme(plot.margin = margin()))
+
+ggsave("figures/scores_by_eval_date_7d.pdf", width = 164, height = 50, unit = "mm", device = "pdf")
